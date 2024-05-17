7e9b833a85a2ea510c8b06734f2251bd670b5213
Timirey <misiru.artiom@gmail.com>
2024-05-17 18:05:03 +0300

    Add crossover and crossunder method helpers

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -25,3 +25,25 @@ if (! function_exists('array_merge_recursive_distinct')) {
         return $merged;
     }
 }
+
+if (! function_exists('crossover')) {
+    function crossover(array $x, array $y): bool
+    {
+        if (empty($y) || empty($x)) {
+            return false;
+        }
+
+        return prev($x) <= prev($y) && end($x) > end($y);
+    }
+}
+
+if (! function_exists('crossunder')) {
+    function crossunder(array $x, array $y): bool
+    {
+        if (empty($y) || empty($x)) {
+            return false;
+        }
+
+        return prev($x) >= prev($y) && end($x) < end($y);
+    }
+}
